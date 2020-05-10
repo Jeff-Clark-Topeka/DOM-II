28188b68ad98369c12fef6398bb0fa33603d5a1d
Jeff Clark <jeffclarktopeka@gmail.com>
2020-05-10 15:05:54 -0500

    added a mouseover and mouseout event listener to the intro image.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+const funBusImg = document.querySelector('.intro img');
+
+funBusImg.addEventListener('mouseover', () => {
+    funBusImg.style.transform = 'scale(1.2)'
+    funBusImg.style.transition = 'all 0.3s'
+});
+
+funBusImg.addEventListener('mouseout', () => {
+    funBusImg.style.transform = 'scale(1.0)'
+});
