553d288aa80f1601b248dcddc055230473a7a897
Jeff Clark <jeffclarktopeka@gmail.com>
2020-05-10 15:50:02 -0500

    added alt image on resize to image right of LETS GO! and added another alt image on scroll for the image left of Adventure Awaits

--- js/index.js
+++ js/index.js
@@ -47,3 +47,16 @@ const awaitsContextMenu = document.querySelectorAll('.text-content h2').item(1);
 awaitsContextMenu.addEventListener('contextmenu', (event) => {
     event.preventDefault();
 });
+
+window.addEventListener('resize', () => {
+
+    const funBusAltImg = document.querySelector('.img-content img');
+    funBusAltImg.src = 'https://images.unsplash.com/photo-1516503424803-708327384b90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+});
+
+window.addEventListener('scroll', () => {
+
+    const funBusAltImg2 = document.querySelectorAll('.img-content img').item(1);
+    funBusAltImg2.src = 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60'
+});
+
